098329ab1f26d90122a0a08fc667c337f2256ee9
Beyer Benjamin <813615@stma.k12.mn.us>
2021-04-01 10:14:25 -0500

    comparisons #1

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-#WRITE YOUR CODE IN THIS FILE
+def greaterThan(a , b):
+    if a > b:
+        return True
+    else:
+         False 
+
+def lessThan(a, b):
+    if a < b:
+        return True
+    else:
+        return False
+
+def equalTo(a, b):
+    if a == b:
+        return True
+    else:
+        return False
+
+def greaterOrEqual(a, b):
+    if a > or == b:
+        return True
+    else:
+        return False
+
+print(greaterOrEqual(8, 8))
